d1e4b9d68dad31ceef41707d3a62e91be38c777b
thevoiceofthemarketatschool <kaleo.mercado@student.csd509j.net>
2025-02-04 15:15:13 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,2 @@
-let listofwords = ["", "", "", ""]
+let listofwords = ["minecraft","iron","gold","diamond","emerald","redstone","lapis","netherite","copper","sword","pickaxe","axe","shovel","hoe","pig","golem","cat","wolf"]
 
