5acc8e12157b4365faa322a4174f3f1599313010
StellaFarley3453 <stella.farley@student.csd509j.net>
2025-02-04 15:36:40 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,18 @@
 let listofwords = ["minecraft","iron","gold","diamond","emerald","redstone","lapis","netherite","copper","sword","pickaxe","axe","shovel","hoe","pig","golem","cat","wolf"]
 
+// south of this line is stella's territory
+let playagain:boolean = true
+game.showLongText("This is a guessing game. It's hangman style, so you guess one letter at a time, and you can only get the letters wrong 6 times, and you fail on the seven. Only single words at a time, all lowercase, nouns only, and here's the kicker: its minecraft themed. Anything that would be 2 words has been shortened to 1.", DialogLayout.Bottom)
+while(playagain) {
+    let failures:number = 0
+    let randomentry = listofwords[randint(0,listofwords.length-1)]
+    let knownthusfar:string[] = []
+    for (let i = 0;i < randomentry.length;i++){
+        knownthusfar.push("_")
+    }
+    while (failures<7) {
+        for(let i = 0;i<listofwords.length;i++) {
+            
+        }
+    }
+}
