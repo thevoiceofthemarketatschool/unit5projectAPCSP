e5cdda730ec3ee2b3ca8f3f706909486ee36fabe
StellaFarley3453 <stella.farley@student.csd509j.net>
2025-02-04 17:21:10 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,17 @@ let listofdead = ["agent","batato"]
 
 
 // south of this line is stella's territory
+
+//these are variables
 let playagain:boolean = true
 let failures: number = null
 let randomentry = null
 let knownthusfar: string[] = null
 let knownfull: string = null
 let succeeded:boolean = null
-
+//this text explains the game rules.
 game.showLongText("This is a guessing game. It's hangman style, so you guess one letter at a time, and you can only get the letters wrong 6 times, and you fail on the seven. Only single words at a time, all lowercase, nouns only, and here's the kicker: its minecraft themed. Anything that would be 2 words has been shortened to 1.", DialogLayout.Bottom)
+// this function takes a string array and combines all the elements into one word, then returns it.
 function calculateknown(model:string[]){
     let output = ""
     for (let i = 0; i < model.length; i++) {
@@ -18,22 +21,27 @@ function calculateknown(model:string[]){
     }
     return output
 }
+//everything is in a while loop so that you can play the game again without being explained the rules again. 
 while(playagain) {
     succeeded = false
     failures = 0
+    
+    // this chooses a random entry in the array for the player to guess
     randomentry = listofwords[randint(0,listofwords.length-1)]
+    
+    //this creates the string array of unknown letters to later be modified as they find letters
     knownthusfar = []
     for (let i = 0;i < randomentry.length;i++){
         knownthusfar.push("_")
     }
-    //testing {
-    game.splash(randomentry)
     knownfull = calculateknown(knownthusfar)
-    game.splash(knownfull)
-    // }
+    //the conditions of the loop cause it to stop asking you for guesses once youve run out of guesses or guessed all the letters
     while (failures<7 && !succeeded) {
         let matches = 0
+        //this asks for a guess and stores it in a variable
         let guess = game.askForString(knownfull,1)
+        
+        //this compares their guess to each individual letter of the answer
         for(let i = 0;i<listofwords.length;i++) {
             if (randomentry.charAt(i) == guess){
                 knownthusfar[i] = randomentry.charAt(i)
@@ -41,15 +49,20 @@ while(playagain) {
                 matches++
                 }
             }
+        
+        //this checks if their guess was in the word, and increases failures if not 
         if (matches < 1){
             failures++
             game.splash("Wrong, " + (7-failures) + " guesses left.")
         }
+        
+        //this prompts the player to play again if they succeed, and also ends the questioning loop
         if (knownfull == randomentry){
             playagain = game.ask("Correct! play again?")
             succeeded = true
         }
     }
+    //this tells the player they lost if they run out of guesses, and prompt them to play again.
     if (failures > 6) {
         game.splash("dang, it looks like you got it wrong.")
         playagain = game.ask("play again?")
